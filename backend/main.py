30f0c233d161d8f59fc7d1bd863c838d9d2e69ce
Grace Bong <gracebong.park@gmail.com>
2024-01-05 16:06:46 -0500

    boilerplate added

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = ['https://localhost:3000']
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",
+        host='localhost',
+        reload=True,
+        port=8000,
+    )
