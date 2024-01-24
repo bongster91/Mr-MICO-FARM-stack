c883f392cf703589161d8da5cdc3ebd1e7165d82
Grace Bong <gracebong.park@gmail.com>
2024-01-24 10:36:13 -0500

    wip

--- frontend/src/Modal/ModalRequest.tsx
+++ frontend/src/Modal/ModalRequest.tsx
@@ -13,5 +13,30 @@ export const handleRequest = async (type: string, request: string, formValues: a
     const account = findAccountType(type, formValues);
     const response = await fetchRequest(request, account.type, formValues);
 
-    return response.status === 201 ? 'Success' : 'Failure';
+    return response.status === 200 ? 'Success' : 'Failure';
+};
+
+export const findType = (props: any) => {
+    return props['assets_id'] ? 'assets' : 'debts';
+};
+
+const findID = (object: any) => {
+    for (let asset of assetTypes) {
+        if (object[ `${asset.type}_id` ]) {
+            return object[ `${asset.type}_id` ];
+        };
+    };
+
+    for (let debt of debtTypes) {
+        if (object[ `${debt.type}_id` ]) {
+            return object[ `${debt.type}_id` ];
+        };
+    };
+};
+
+export const handlePutRequest = async (type: string, request: string, formValues: any, props: any) => {
+    const id = findID(props);
+    const response = await fetchRequest(request, type, formValues, id);
+
+    return response.status === 200 ? 'Success' : 'Failure';
 };
---
+++ frontend/src/Table/TableModal.tsx
@@ -0,0 +1,138 @@
+import React, { useState } from 'react';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import{ Box }from '@mui/material';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import MenuItem from '@mui/material/MenuItem';
+import Backdrop from '@mui/material/Backdrop';
+import { useTheme } from '@mui/material';
+import { Typography } from '@mui/material';
+import Alert from '@mui/material/Alert';
+
+import { assetTypes, debtTypes } from '../Modal/types';
+import { handleRequest, findAccountType, findType, handlePutRequest } from '../Modal/ModalRequest';
+
+
+function TableModal({props, handleSuccessAlert}: any){
+    console.log('PROPS: ', props);
+    const { name, amount, type } = props;
+    const generalType = findType(props);
+    const [isModalOpen, setIsModalOpen] = useState(true);
+    const [formValue, setFormValue] = useState({
+        name: name,
+        type: type,
+        amount: amount
+    });
+
+    const theme = useTheme();
+
+    const handleModalOpen = () => {
+        setIsModalOpen(!isModalOpen);
+    };
+
+    const handleChange = (e: any) => {
+        if (generalType === 'assets') {
+            e.target.id 
+            ?
+                setFormValue((prev) => ({ ...prev, [e.target.id]: e.target.value, assets_id: 1 })) 
+            : 
+                setFormValue((prev) => ({ ...formValue, [e.target.name]: e.target.value, assets_id: 1 })); 
+        } else {
+            e.target.id 
+            ?
+                setFormValue((prev) => ({ ...prev, [e.target.id]: e.target.value, debts_id: 1 })) 
+            : 
+                setFormValue((prev) => ({ ...formValue, [e.target.name]: e.target.value, debts_id: 1 })); 
+        };
+    };
+
+    const handleSubmit = async(e: any) => {
+        e.preventDefault();
+        
+        const accountType = findAccountType(generalType, formValue);
+        const response = await handlePutRequest(generalType, 'PUT', formValue, props);
+        
+        return handleSuccessAlert(response);
+    };
+
+    return (
+        <Modal
+            className='modal'
+            open={isModalOpen}
+            onClose={handleModalOpen}
+            closeAfterTransition
+            slots={{ backdrop: Backdrop }}
+            slotProps={{
+                backdrop: {
+                    timeout: 500,
+                    style: {
+                        backgroundColor: 'rgba(255,255,255,0.2)',
+                    }
+                }
+            }}
+        >
+            <Box 
+                className='modal-form-box'
+                component={'form'} 
+            >
+                <TextField
+                    required
+                    id="name"
+                    label="Title"
+                    onChange={handleChange}
+                    value={formValue.name}
+                />
+                <TextField
+                    id='type'
+                    className='modal-dropdown'
+                    name='type'
+                    select
+                    label='Type'
+                    onChange={handleChange}
+                    value={formValue.type}
+                    defaultValue={generalType === 'assets' ? 'Checking' : 'Household'}
+                >
+                    {
+                        generalType === 'assets' 
+                        ?
+                            assetTypes.map((asset, index) => {
+                                return <MenuItem key={index} value={asset.value}>{asset.label}</MenuItem>;
+                            })
+                        :
+                            debtTypes.map((debt, index) => {
+                                return <MenuItem key={index} value={debt.value}>{debt.label}</MenuItem>;
+                            })
+                    }
+                </TextField>
+                <TextField
+                    required
+                    id="amount"
+                    label="Amount"
+                    onChange={handleChange}
+                    value={formValue.amount}
+                />
+                <Button 
+                    className='modal-submit-button'
+                    onClick={handleSubmit} 
+                    style={{
+                        backgroundColor: theme.palette.primary.dark,
+                        outlineColor: theme.palette.primary.main
+                    }}>
+                        Submit
+                </Button>
+                <Button 
+                    className='modal-cancel-button'
+                    onClick={handleModalOpen} 
+                    style={{
+                        backgroundColor: theme.palette.primary.light,
+                        outlineColor: theme.palette.primary.main
+                    }}>
+                        Cancel
+                </Button>
+            </Box>
+        </Modal>
+    );
+}
+
+export default TableModal;
